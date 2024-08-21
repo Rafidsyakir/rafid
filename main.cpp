6433501e3ba119f9592023d1c2f5d68afb4ae8fb
Rafidsyakir <rafidsyakir30@gmail.com>
2024-08-21 11:29:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program Variabel!" << endl;
+    int a;
+    float b;
+    float hasil;
+    string kt="Siswa kelas X RPL";
+    string pr="Belajar Bahasa CPP"
+    char d="*"
+    int e=1;
+    int f=2;
+    int a=15;
+    int b=25;
+
+    cout<<"nilai a adalah :"<<a<<"\n \n";
+    cout<<"nilai b adalah :"<<b<<" \n"
+    cout<<"hasil penjumlahan a+b adalah :"<<a+b<<endl;
+    cout<< kt+d+ pr<<endl;
+    cout<< e +f;
+
+    return 0;
+}
